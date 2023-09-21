7d5052551de2ca2790985dcc73cd111bd221373b
Thomas12012002 <65460024+Thomas12012002@users.noreply.github.com>
2023-09-21 15:27:01 -0400

    Update MinQueue.h

--- MinQueue.h
+++ MinQueue.h
@@ -14,7 +14,7 @@ private:
     T* heapArr;
     int length{};
     int heap_size{};
-    int* position;
+    int* position; // Array to store position
     //int parent(int i){return i/2;} //+1?
     int left(int i){return 2*i + 1;}
     int right(int i){return 2*i + 2;}
