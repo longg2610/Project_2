13e42400f42c577eacf397c004a6a8b6461b1e6f
Thomas12012002 <65460024+Thomas12012002@users.noreply.github.com>
2023-09-21 16:09:23 -0400

    Update usecase.cpp

--- usecase.cpp
+++ usecase.cpp
@@ -6,17 +6,17 @@
 using namespace std;
 
 template <typename T>
-vector<T> MinQueue<T>::minSlidingWindow(T arr[], int len, int k) {
+vector<T> minSlidingWindow(T arr[], int len, int window) {
     vector<T> result;
-    MinQueue_version2<T> MinQueue_version2(k);
+    MinQueue_version2<T> MinQueue_version2(window);
 
-    for (int i = 0; i < k; ++i) {
+    for (int i = 0; i < window; ++i) {
         MinQueue_version2.insert_version2(arr[i], i);
     }
     result.push_back(MinQueue_version2.min());
 
-    for (int i = k; i < len; ++i) {
-        MinQueue_version2.remove(i - k);
+    for (int i = window; i < len; ++i) {
+        MinQueue_version2.remove(i - window);
         MinQueue_version2.insert_version2(arr[i], i);
         result.push_back(MinQueue_version2.min());
     }
